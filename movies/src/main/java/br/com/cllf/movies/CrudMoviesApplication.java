22ca3b5ede7a3dc02879d933f867d647940c3b9d
caiodallavechia <caio.dallavechia@gmail.com>
2023-01-26 22:19:26 -0300

    Initial Setup

---
+++ movies/src/main/java/br/com/cllf/movies/CrudMoviesApplication.java
@@ -0,0 +1,13 @@
+package br.com.cllf.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMoviesApplication.class, args);
+	}
+
+}
